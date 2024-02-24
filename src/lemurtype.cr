52ce198cde67f1b1ac07ed7ca8a47cceb8a4be4a
cyanophage <69594940+cyanophage@users.noreply.github.com>
2024-02-24 17:53:50 +0000

    new practice mode

--- src/lemurtype.cr
+++ src/lemurtype.cr
@@ -174,11 +174,15 @@ class Database
   property words : Hash(String, Record)
   property history : Hash(Int64, Float64)
   property letters : Hash(String, Hash(String, Int32|Float64))
+  property layout : Array(Array(String))
+  property level : Int32
 
   def initialize
     @words = {} of String => Record
     @history = {} of Int64 => Float64
     @letters = {} of String => Hash(String, Int32|Float64)
+    @layout = [] of Array(String)
+    @level = 1
   end
 
   def add_word(word)
@@ -234,6 +238,8 @@ class Typing
     @store = "database-#{File.basename(@word_list)}.yaml"
     @database = Database.new
     @sentence = Sentence.new
+    @bigrams = {} of String => Hash(String, Int32)
+    @trigrams = {} of String => Hash(String, Int32)
     @mode = :options
     @cursor_row = 0
     @cursor_col = 0
@@ -246,10 +252,16 @@ class Typing
     @max = Time.utc(1970, 1, 1)
     @chars = 0
     @log = ""
+    @database.layout = [] of Array(String)
+    @database.layout << ["q","w","e","r","t","y","u","i","o","p","-"]
+    @database.layout << ["a","s","d","f","g","h","j","k","l",";","'"]
+    @database.layout << ["z","x","c","v","b","n","m",",",".","/"]
+
   end
 
   def load_words
     if File.exists?(@store)
+      puts "loading from #{@store}"
       lines = [] of String
       File.each_line(@store) do |line|
         lines << line
@@ -262,6 +274,8 @@ class Typing
         @database.add_word(word)
       end
     end
+    @bigrams = Hash(String,Hash(String,Int32)).from_yaml(File.read("freqs.yaml"))
+    @trigrams = Hash(String,Hash(String,Int32)).from_yaml(File.read("trifreqs.yaml"))
   end
 
   def draw()
@@ -290,15 +304,17 @@ class Typing
     end
   end
 
-  def show_results()
+  def show_results(save)
     NCurses.clear
     NCurses.set_color 1
     row = 6
     col = 10
-    wpm = calculate_wpm()
-    @database.history[Time.utc.to_unix] = wpm
-    write_database()
-    NCurses.print("Words per minute:     #{wpm}", row, col)
+    if save
+      wpm = calculate_wpm()
+      @database.history[Time.utc.to_unix] = wpm
+      write_database()
+      NCurses.print("Words per minute:     #{wpm}", row, col)
+    end
     row += 2
     letters = 0
     errors = 0
@@ -336,6 +352,65 @@ class Typing
     row += 1
   end
 
+  def show_practice_results()
+    NCurses.clear
+    NCurses.set_color 1
+    row = 6
+    col = 10
+    threshold = 0.5 # ~35 wpm
+    NCurses.print("Time per character:", row, col)
+    NCurses.print("Level: #{@database.level}", row+1, col)
+    row += 2
+    speed = {} of String => Float64
+    counts = {} of String => Int32
+    errors = {} of String => Int32
+    fast_enough = {} of String => Int32
+    letter_list = @sentence.get_all()
+    (1..letter_list.size-1).each do |i|
+      char = letter_list[i][0]
+      before = letter_list[i-1][1][0]
+      after = letter_list[i][1][-1]
+      diff = (after - before).to_f # time in seconds
+      speed[char.to_s] ||= 0.0
+      speed[char.to_s] += diff
+      counts[char.to_s] ||= 0
+      counts[char.to_s] += 1
+      fast_enough[char.to_s] ||= 0
+      fast_enough[char.to_s] += 1 if diff < threshold
+      if letter_list[i][2].size == 1 && letter_list[i][2][0] == char
+      else
+        errors[char.to_s] ||= 0
+        errors[char.to_s] += 1
+      end
+    end
+    NCurses.print("Speeds:", row, col)
+    row += 1
+    NCurses.set_color
+    beat = 0
+    speed.to_a.sort_by{|x| x[0]}.to_h.each do |char, sec|
+      next if char == ' ' || char == " "
+      good = fast_enough[char]/counts[char]
+      if good > 0.9
+        star = "*"
+        beat += 1
+      else
+        star = " "
+      end
+      if errors.has_key?(char)
+        err = errors[char]
+      else
+        err = 0
+      end
+      NCurses.print("  #{char}#{" "*16}#{(sec/counts[char]).round(2)} (#{(60*counts[char]/sec).round(0)}) #{star} #{good.round(2)} errors: #{err}", row, col)
+      row += 1
+    end
+    if beat >= speed.keys.size - 1
+      @database.level += 1
+      NCurses.print("Increasing level", row+2, col)
+    end
+    write_database()
+  end
+
   def update_database()
     @sentence.words.each do |word|
       dur = word.duration
@@ -370,12 +445,12 @@ class Typing
     end
     char_stats.each do |char, stats|
       @database.add_letter(char, stats)
-      @log += "#{char}\t#{stats["count"]}\t#{stats["time"]}\t#{stats["errors"]}\n"
+      # @log += "#{char}\t#{stats["count"]}\t#{stats["time"]}\t#{stats["errors"]}\n"
     end
 
   end
 
-  def letter_practice(ch)
+  def letter_practice(ch) # picking words that contain the letter 'ch' to practice on
     NCurses.clear
     @sentence = Sentence.new
     @min = Time.utc(2100, 1, 1)
@@ -396,6 +471,95 @@ class Typing
     end
     draw()
     set_cursor()
+  end
+
+  def create_practice_bigrams(n) # basic keybr style practicing
+    # letters in order
+    order = ['e','t','a','o','i','n','s','r','h','l','d','c','u','m','f','p','g','y','w','b','.','v',',','k','-','\'','x','j','q','z']
+    letters = [] of String
+    (0..@database.level).each do |i|
+      letters << order[i].to_s
+    end
+    @sentence = Sentence.new
+    sel = "e"
+    n.times do
+      l = rand(2..5)
+      word = ""
+      sel = letters.sample
+      word += sel
+      while word.size < l
+        bigrams = {} of String => Int32
+        @bigrams[sel].each do |letter, freq|
+          if letters.includes?(letter)
+            bigrams[letter] = freq
+          end
+        end
+
+        total = bigrams.values.sum
+        ran = rand(1..total)
+        cum = 0
+        bigrams.each do |letter, freq|
+          cum += freq
+          if ran <= cum
+            sel = letter
+            break
+          end
+        end
+        word += sel
+      end
+      @sentence.add_word(word)
+    end
+    # keep track of speed and accuracy and only increase the level when ready
+    # can we calculate accuracy per letter?
+  end
+
+  def create_practice(n)
+    order = ['e','t','a','o','i','n','s','r','h','l','d','c','u','m','f','p','g','y','w','b','.','v',',','k','-','\'','x','j','q','z']
+    letters = [] of String
+    (0..@database.level).each do |i|
+      letters << order[i].to_s
+    end
+    @sentence = Sentence.new
+    # print "letters: "
+    # p letters
+    subtris = {} of String => Hash(String, Int32)
+    @trigrams.each do |bigram, hash|
+      if letters.includes?(bigram[0].to_s) && letters.includes?(bigram[1].to_s)
+        # puts bigram
+        hash.each do |letter, count|
+          if letters.includes?(letter)
+            subtris[bigram] ||= {} of String => Int32
+            subtris[bigram][letter] = count
+          end
+        end
+      end
+    end
+    # p subtris
+    # abort
+    n.times do
+      l = rand(3..6)
+      word = subtris.keys.sample
+      sel = "."
+      while word.size < l
+        bigram = word[-2..-1]
+        if subtris.has_key?(bigram)
+          total = subtris[bigram].values.sum
+          ran = rand(1..total)
+          cum = 0
+          subtris[bigram].each do |letter, freq|
+            cum += freq
+            if ran <= cum
+              sel = letter
+              break
+            end
+          end
+          word += sel
+        else
+          word += letters.sample
+        end
+      end
+      @sentence.add_word(word)
+    end
   end
 
   def create_sentence(n)
@@ -502,6 +666,16 @@ class Typing
     @pc_row = @sentence.words[0].row + 1
   end
 
+  def start_practice()
+    @min = Time.utc(2100, 1, 1)
+    @max = Time.utc(1970, 1, 1)
+    @chars = 0
+    create_practice(@test_length)
+    draw()
+    set_cursor()
+    @pc_row = @sentence.words[0].row + 1
+  end
+
   def typing_test(ch)
     wpm = 0.0f64
     if ch.is_a?(Char)
@@ -509,13 +683,26 @@ class Typing
       if !@sentence.type_char(ch)
         update_database()
         @mode = :results
-        show_results()
+        show_results(true)
         return
       end
       wpm = calculate_wpm()
       if @max > @min
         NCurses.set_color
         NCurses.print("wpm: #{wpm}", 4, 10)
+      end
+      set_cursor()
+    end
+  end
+
+  def practice_test(ch)
+    wpm = 0.0f64
+    if ch.is_a?(Char)
+      draw_char(ch)
+      if !@sentence.type_char(ch)
+        @mode = :results
+        show_practice_results()
+        return
       end
       set_cursor()
     end
@@ -531,7 +718,9 @@ class Typing
         minutes = 0
       end
       # speed = 40
-      if @database.history.values.size < 10
+      if @database.history.values.size == 0
+        speed = 10
+      elsif @database.history.values.size < 10
         speed = @database.history.values.sum/@database.history.values.size
       else
         speed = @database.history.values[-10..-1].sum/10.0
@@ -593,17 +782,17 @@ class Typing
       end
     end
     row += 1
-    NCurses.print("1 sort by letter",row,col)
+    NCurses.print("1) sort by letter",row,col)
     row += 1
-    NCurses.print("2 sort by count",row,col)
+    NCurses.print("2) sort by count",row,col)
     row += 1
-    NCurses.print("3 sort by errors",row,col)
+    NCurses.print("3) sort by errors",row,col)
     row += 1
-    NCurses.print("4 sort by speed",row,col)
+    NCurses.print("4) sort by speed",row,col)
     row += 1
-    NCurses.print("a-z to practice on that letter",row,col)
+    NCurses.print("a-z) to practice on that letter",row,col)
     row += 1
-    NCurses.print("esc to go back to the main menu",row,col)
+    NCurses.print("Esc) go back to the main menu",row,col)
   end
 
   def history()
@@ -619,25 +808,44 @@ class Typing
     n = 0
     history.each do |time, wpm|
       t = Time.unix(time)
-      NCurses.print("#{t.year}-#{"%02d" % t.month}-#{"%02d" % t.day} #{idx} : #{wpm}", row, col)
       sigma_x += idx
       sigma_x2 += (idx*idx)
       sigma_y += wpm
       sigma_xy += wpm * idx
       n += 1
       idx += 1
-      row += 1
-      if row > 50
-        row = 6
-        col += 30
-      end
     end
     if n * sigma_x2 - sigma_x * sigma_x != 0
 			a = (sigma_y*sigma_x2 - sigma_x*sigma_xy) / (n*sigma_x2 - sigma_x*sigma_x)
 			b = (n*sigma_xy - sigma_x*sigma_y) / (n*sigma_x2 - sigma_x*sigma_x)
 
       NCurses.print("Improving by #{(10*b).round(2)} wpm every 10 tests", 3, 10)
-      NCurses.print("a = #{a.round(1)}, b = #{b.round(3)}", 4, 10)
+      # NCurses.print("a = #{a.round(1)}, b = #{b.round(3)}", 4, 10)
+    end
+    now = Time.utc.to_unix
+    list = {} of String => Float32
+    counts = {} of String => Int32
+    history.each do |time, wpm|
+      t = Time.unix(time)
+      if time < now - 86400
+        day = "#{t.year}-#{"%02d" % t.month}-#{"%02d" % t.day}"
+      else
+        day = "#{t.year}-#{"%02d" % t.month}-#{"%02d" % t.day} #{"%02d"%t.hour}:#{"%02d"%t.minute}:#{"%02d"%t.second}"
+      end
+      list[day] ||= 0
+      list[day] += wpm
+      counts[day] ||= 0
+      counts[day] += 1
+    end
+
+    list.each do |day, wpm|
+      avg = (wpm / counts[day]).round(1)
+      NCurses.print("#{day} : #{avg}", row, col)
+      row += 1
+      if row > 50
+        row = 6
+        col += 30
+      end
     end
   end
 
@@ -649,6 +857,7 @@ class Typing
       total_visit += record.typed
     end
     scores = {} of String => Float64
+    visits = {} of String => Int32
     @database.words.each do |word, record|
       word_visit = record.typed
       word_error = record.errors
@@ -660,6 +869,7 @@ class Typing
         score = spc + (word_error/word_visit) + 1.414 * Math.sqrt(Math.log(total_visit)/word_visit)
       end
       scores[word] = score
+      visits[word] = word_visit
     end
     scores = scores.to_a.sort_by{|x| x[1]}
     rscores = scores.to_a.sort_by{|x| -x[1]}
@@ -684,6 +894,19 @@ class Typing
       break if row > 50
     end
 
+    # if visits.size > 0
+    #   col = 80
+    #   row = 6
+    #   NCurses.print("Unknown:",row-2,col)
+    #   visits.each do |word,count|
+    #     if count == 0
+    #       NCurses.print("#{word}#{" "*(20-word.size)}#{count}",row, col)
+    #       row += 1
+    #     end
+    #     break if row > 50
+    #   end
+    # end
+
   end
 
   def options_menu()
@@ -692,10 +915,64 @@ class Typing
     row = 6
     NCurses.set_color
     NCurses.print("1) Typing test", row, col)
-    NCurses.print("2) Word stats", row+1, col)
-    NCurses.print("3) Letter stats", row+2, col)
-    NCurses.print("4) History", row+3, col)
-    NCurses.print("q) Quit", row+4, col)
+    NCurses.print("2) Practice mode", row+1, col)
+    NCurses.print("3) Word stats", row+2, col)
+    NCurses.print("4) Letter stats", row+3, col)
+    NCurses.print("5) History", row+4, col)
+    NCurses.print("s) Settings", row+5, col)
+    NCurses.print("q) Quit", row+6, col)
+  end
+
+  def settings_menu()
+    NCurses.clear
+    col = 10
+    row = 6
+    NCurses.set_color
+    NCurses.print("1) Define layout", row, col)
+    NCurses.print("Esc) Back to main menu", row+5, col)
+  end
+
+  def layout_def()
+    NCurses.clear
+    col = 10
+    row = 6
+
+    col2 = 0
+    row2 = 0
+    NCurses.set_color
+    (0..2).each do |r|
+      col = 10
+      @database.layout[r].each_with_index do |ch,c|
+        NCurses.print("#{ch}", row, col)
+        if @cursor_row == r && @cursor_col == c
+          # @log += "<744> #{r} #{c}\n"
+          row2 = row
+          col2 = col
+        end
+        col += 2
+        if col == 20
+          col += 4
+        end
+      end
+      row += 2
+    end
+    # problems
+    row = 12
+    col = 10
+    problems = 0
+    (('a'..'z').to_a<<'\'').each do |letter|
+      if @database.layout.flatten.join("").count(letter) > 1
+        NCurses.print("found more than one #{letter.to_s}", row, col)
+        row+=1
+      elsif @database.layout.flatten.join("").count(letter) < 1
+        NCurses.print("found no #{letter.to_s}", row, col)
+        row+=1
+      end
+    end
+    row += 1
+    NCurses.print("double tap Esc to go back to settings",row,col)
+
+    NCurses.move(row2, col2)
   end
 
   def start
@@ -717,14 +994,20 @@ class Typing
           @mode = :typing
           start_typing_test()
         when '2'
+          @mode = :practice
+          start_practice()
+        when '3'
           @mode = :wordstats
           word_stats()
-        when '3'
+        when '4'
           @mode = :letterstats
           letter_stats(1)
-        when '4'
+        when '5'
           @mode = :history
           history()
+        when 's'
+          @mode = :settings
+          settings_menu()
         when 'q'
           break
         end
@@ -736,6 +1019,13 @@ class Typing
         else
           pacing_cursor()
           typing_test(ch)
+        end
+      when :practice
+        if ch.to_s == "Esc"
+          @mode = :options
+          options_menu()
+        else
+          practice_test(ch)
         end
       when :results
         @mode = :options
@@ -764,13 +1054,57 @@ class Typing
           @mode = :options
           options_menu()
         end
+      when :settings
+        if ch.to_s == "Esc"
+          @mode = :options
+          options_menu()
+        elsif ch == '1'
+          @mode = :layout
+          @cursor_row = 0
+          @cursor_col = 0
+          layout_def()
+          NCurses.keypad true
+        end
+      when :layout
+        if ch.to_s == "Esc"
+          @mode = :settings
+          NCurses.keypad false
+          settings_menu()
+        elsif ch.to_s == "Left"
+          @cursor_col -= 1
+        elsif ch.to_s == "Right"
+          @cursor_col += 1
+        elsif ch.to_s == "Up"
+          @cursor_row -= 1
+        elsif ch.to_s == "Down"
+          @cursor_row += 1
+        else
+          if ch.is_a?(Char)
+            @database.layout[@cursor_row][@cursor_col] = ch.to_s
+            write_database()
+            @cursor_col += 1
+            if @cursor_col > 10
+              @cursor_col = 0
+              @cursor_row += 1
+            end
+          end
+        end
+        if @mode == :layout
+          @cursor_row = 0 if @cursor_row < 0
+          @cursor_row = 2 if @cursor_row > 2
+          @cursor_col = 0 if @cursor_col < 0
+          @cursor_col = 10 if @cursor_col > 10
+          layout_def()
+        end
       end
     end
 
     NCurses.end
+    # puts @log
   end
 
 end
 
 typer = Typing.new
+# typer.load_words()
 typer.start
